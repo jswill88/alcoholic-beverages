8a9227b08ef9e0cfb8686221378e5b3d78e37769
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-04-16 12:27:04 -0700

    added functions

--- app.js
+++ app.js
@@ -1,30 +1,44 @@
 var age = prompt("How old are you?")
-if (age < 21) {
-    document.write('<h1 id="young">YOU ARE TOO YOUNG. PLEASE LEAVE THIS SITE</h1><!--')
-} else {
-var howMany = prompt("How many beers do you drink each day?")
-
-if (howMany > 9) {
-    document.write('<h3>Please visit <a href="https://www.aa.org/">Alcoholics Anonymous</a></h3>');
-} else {
-
 var today = new Date();
 var hourNow = today.getHours();
 var minutesLeft = 60 - today.getMinutes();
 var shouldDrink;
 
-if (hourNow >= 17) {
-    shouldDrink = "It's already past 5pm, time to drink!";
-} else {
-   var timeToDrink = 16 - hourNow;
-   shouldDrink = "You should start drinking in " + timeToDrink + " hours and "
-   + minutesLeft + " minutes!";
-}
-if (howMany > 4) {
-    document.write('<h3>You like to party!</h3>');
-} else {
-    document.write('<h3>You seem like a reasonable drinker.</h3>');
+function tooYoung() {
+    document.write('<h1 id="young">YOU ARE TOO YOUNG. PLEASE LEAVE THIS SITE</h1><!--')
 }
 
-document.write('<h3>' + shouldDrink + '</h3>');
-}}
+function alcoholic() {
+    document.write('<h3>Please visit <a href="https://www.aa.org/">Alcoholics Anonymous</a></h3>');
+}
+
+function whenToDrink() {
+    if (hourNow >= 17) {
+        shouldDrink = "It's already past 5pm, time to drink!";
+    } else {
+    var timeToDrink = 16 - hourNow;
+    shouldDrink = "You should start drinking in " + timeToDrink + " hours and "
+    + minutesLeft + " minutes!";
+    }
+    document.write('<h3>' + shouldDrink + '</h3>');
+} 
+
+function userDescription() {
+    if (howMany > 4) {
+        document.write('<h3>You like to party!</h3>');
+    } else {
+        document.write('<h3>You seem like a reasonable drinker.</h3>');
+    }
+}
+
+if (age > 21) {
+    tooYoung();
+} else {
+    var howMany = prompt("How many beers do you drink each day?")
+    if (howMany > 10) {
+        alcoholic();
+    } else {
+        whenToDrink();
+    }
+    userDescription();
+}
